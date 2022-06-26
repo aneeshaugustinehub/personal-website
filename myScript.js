989565b49e6a41ffee93f99999d2fd80d64a1601
Aneesh Augustine <66233785+aneeshaugustinehub@users.noreply.github.com>
2022-06-26 08:30:42 +0530

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,30 @@
+<script>
+        $("#submit-form").submit((e)=>{
+            e.preventDefault()
+            $.ajax({
+                url:"https://script.google.com/macros/s/AKfycbwagLP_m2FdV6o4wWvBeXRoXuW107hgm0Rha3hp_q9nJupwlkPV0Bv5QSEfiz8Wi018sQ/exec",
+                data:$("#submit-form").serialize(),
+                method:"post",
+                success:function (response){
+                    alert("Form submitted successfully")
+                    window.location.reload()
+                    //window.location.href="https://google.com"
+                },
+                error:function (err){
+                    alert("Something Error")
+    
+                }
+            })
+        })
+    </script>
+
+<script>
+	function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+</script>
